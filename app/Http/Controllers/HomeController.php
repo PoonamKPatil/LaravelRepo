938c568d7dc19f785ffb661ef55427f27204a7eb
Poonam Patil <you@example.com>
2017-12-22 11:49:02 +0530

    used eloquent

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Info;
 use DB;
 
 class HomeController extends Controller
 {
     public function getView()
     {
-    	$info = DB::table('info')->latest()->get();
+    	//$info = DB::table('info')->latest()->get();
 
-    	return view('userInfo/home', compact('info'));
+    	$info = Info::all();
+
+        return view('userInfo/home', compact('info'));
     }
 
     public function getViewById($id)
     {
-    	$info = DB::table('info')->find($id);
+    	//$info = DB::table('info')->find($id);
+
+    	$info = Info::find($id);
 
     	return view('userInfo/showInfo', compact('info'));
     }
