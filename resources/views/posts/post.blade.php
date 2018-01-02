67cfaeb3722efe15620f4e5f64b1e957157ceaa9
Poonam Patil <you@example.com>
2018-01-02 11:37:21 +0530

    relationships between models and comment

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -3,6 +3,9 @@
     <a href="/posts/{{ $post->id }}">
       <h2 class="blog-post-title">{{$post->title}}</h2>
     </a>
-      <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}} </p>{{$post->body}}
+    
+    <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}} </p>
+    <p>{{$post->body}}</p>
+
 
 </div>
